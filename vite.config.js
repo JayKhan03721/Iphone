bcbcf80c668abb943ddc151e9e36a78214917a80
JayKhan03721 <jaykhan03721@gmail.com>
2025-04-06 13:00:28 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Iphone/',
+  // base: '/Iphone',
   server: {
     host: true,  // Expose to the network
   },
